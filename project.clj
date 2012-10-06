8eb31d34fae3098b4b6bdc2394762233f8e27834
Yasuhisa Yoshida <syou6162@gmail.com>
2012-10-06 10:03:57 -0700

    urlとdescriptionを追加

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject fobos_multiclass_clj "0.1.0-SNAPSHOT"
+  :description "Multiclass classifier based on FOBOS"
+  :url "https://github.com/syou6162/fobos_multiclass_clj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [fobos_clj "0.0.2"]]
+  :jvm-opts ["-Xmx3g" "-server" "-Dfile.encoding=UTF-8"]
+  :main fobos-multiclass-clj.core)
