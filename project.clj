426f067fe032361c2247579fdec0b956694f1124
Yasuhisa Yoshida <syou6162@gmail.com>
2012-10-25 19:05:46 +0900

    fobos_cljのversionを上げた

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject fobos_multiclass_clj "0.1.2"
+(defproject fobos_multiclass_clj "0.1.3"
   :description "Multiclass classifier based on FOBOS"
   :url "https://github.com/syou6162/fobos_multiclass_clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [fobos_clj "0.0.3"]]
+                 [fobos_clj "0.0.4"]]
   :jvm-opts ["-Xmx3g" "-server" "-Dfile.encoding=UTF-8"]
   :main fobos-multiclass-clj.core)
